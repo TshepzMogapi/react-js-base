1d3200bb97333ef86f0444c23fd325ebf96c8e73
Tshepiso Mogapi <tshepzmogapi@gmail.com>
2021-05-17 22:16:40 +0200

    basic react componet

---
+++ src/components/Product.js
@@ -0,0 +1,12 @@
+import './Product.css';
+function Product() {
+  return (
+    <div>
+      <div className="data-1">Data 1</div>
+      <div className="data-2">Data 2</div>
+      <div className="data-3">Data 3</div>
+    </div>
+  );
+}
+
+export default Product;
